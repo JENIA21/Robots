7db4fe985deffdd582fd7762e2c0244343c0e98f
CBists <105043288+CBists@users.noreply.github.com>
2022-11-25 00:41:41 +0500

    Устранена утечка памяти тем, что сохранялись подписки(Вроде) Добавлена очередь на сообщения в логе

---
+++ robots/src/log/LogWindowSource.java
@@ -0,0 +1,86 @@
+package log;
+
+import java.util.*;
+
+/**
+ * Что починить:
+ * 1. Этот класс порождает утечку ресурсов (связанные слушатели оказываются
+ * удерживаемыми в памяти)
+ */
+public class LogWindowSource
+{
+    private final int m_iQueueLength;
+    
+    private final Queue<LogEntry> m_messages;
+    private final List<LogChangeListener> m_listeners;
+    private volatile LogChangeListener[] m_activeListeners;
+    
+    public LogWindowSource(int iQueueLength) 
+    {
+        m_iQueueLength = iQueueLength;
+        m_messages = new ArrayDeque<>();
+        m_listeners = new ArrayList<>();
+    }
+    
+    public void registerListener(LogChangeListener listener)
+    {
+        synchronized(m_listeners)
+        {
+            m_listeners.add(listener);
+            m_activeListeners = null;
+        }
+    }
+    
+    public void unregisterListener(LogChangeListener listener)
+    {
+        synchronized(m_listeners)
+        {
+            m_listeners.remove(listener);
+            m_activeListeners = null;
+        }
+    }
+    
+    public void append(LogLevel logLevel, String strMessage)
+    {
+        LogEntry entry = new LogEntry(logLevel, strMessage);
+        if(size() >= m_iQueueLength)
+            m_messages.poll();
+        m_messages.add(entry);
+        LogChangeListener [] activeListeners = m_activeListeners;
+        if (activeListeners == null)
+        {
+            synchronized (m_listeners)
+            {
+                if (m_activeListeners == null)
+                {
+                    activeListeners = m_listeners.toArray(new LogChangeListener [0]);
+                    m_activeListeners = activeListeners;
+                }
+            }
+        }
+        for (LogChangeListener listener : activeListeners)
+        {
+            listener.onLogChanged();
+        }
+    }
+    
+    public int size()
+    {
+        return m_messages.size();
+    }
+
+    public Iterable<LogEntry> range(int startFrom, int count)
+    {
+        if (startFrom < 0 || startFrom >= m_messages.size())
+        {
+            return Collections.emptyList();
+        }
+        int indexTo = Math.min(startFrom + count, m_messages.size());
+        return m_messages.stream().toList().subList(startFrom, indexTo);
+    }
+
+    public Iterable<LogEntry> all()
+    {
+        return m_messages;
+    }
+}
